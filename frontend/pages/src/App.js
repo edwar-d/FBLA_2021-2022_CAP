e99cdb19c0de84a62ed7c6ecf107f3f88a3999fa
Kartik Joshi <kjoshi980@student.fuhsd.org>
2022-02-18 17:35:48 -0800

    added post requests

--- frontend/pages/src/App.js
+++ frontend/pages/src/App.js
@@ -3,14 +3,34 @@ import SearchBar from './Components/SearchBar';
 import { Button, ButtonGroup } from '@material-ui/core';
 import touristData from "./Data.json";
 import styled from 'styled-components';
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import LoginForm from './Components/LoginForm'
 import Flower from './images/stockimag.jpg'
-import displayCards from './Components/displayTourists'
+import Grid from "@material-ui/core/Grid";
+import TouristCards from './Components/displayTourists';
 
 
+
+// const todoItems = [
+//   {
+//     id: 1,
+//     city: "San Jose",
+//     rating: "4",
+//     type: "restaurant",
+//   },
+// ];
+
+const requestOptions = {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ City: '', Rating: '', Type: '',})
+};
+fetch('http://127.0.0.1:8000/api/v1/json/call', requestOptions)
+  .then(response => response.json())
+  .then(data => this.setState({ postId: data.id }));
 function App() {
   return (
+
 
     <div>
       <div className='image'>
@@ -18,8 +38,14 @@ function App() {
        style={{width: 800, height: 800, padding: 1, marginLeft: 875, marginTop: 60}} /> } 
       </div>
       <div className='App'>
-        <displayCards></displayCards>
       <LoginForm></LoginForm>
+      </div>
+      <div className='Cards'>
+        <Grid container>
+          <Grid item xs={6}>  
+          {/* <TouristCards></TouristCards> */}
+        </Grid>
+        </Grid>
       </div>
     </div>
 
--- frontend/pages/src/Components/LoginForm.js
+++ frontend/pages/src/Components/LoginForm.js
@@ -9,28 +9,45 @@ function LoginForm(){
             <div className="form-inner">
             <h2> Find the perfect location</h2>
             <div className="form-group">
-                <label htmlFor="name"> Cost : </label>
-                <input type="text" name="cost" id="cost"/>
+                <label htmlFor="name"> City: </label>
+                <select name="selectList" id="selectList">
+                <option value="option 1">San Jose </option>
+                <option value="option 2">San Francisco</option>
+                <option value="option 3">Oakland</option>
+                <option value="option 4">Berkeley</option>
+            </select>
             </div>
                 <div className="form-group">
-                    <label htmlFor="Rating">Rate : </label>
-                    <input type="text" name="rating" id="rating"/>
-                </div>
+                    <label htmlFor="Rating">Rating: </label>
+                    <select name="selectList" id="selectList">
+                <option value="option 5">1.0 </option>
+                <option value="option 6">2.0</option>
+                <option value="option 7">3.0</option>
+                <option value="option 8">4.0</option>
+                <option value="option 9">5.0</option>
+            </select>
+                      </div>
                 <div className="form-group">
                     <label htmlFor="Type">Type : </label>
-                    <input type="text" name="type" id="type"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="Time">Time : </label>
-                    <input type="text" name="time" id="time"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="Tour"> Tour : </label>
-                    <input type="text" name="tour" id="tour"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="Distance"> Dist : </label>
-                    <input type="text" name="distance" id="distance"/>
+                    <select name="selectList" id="selectList">
+                <option value="option 10">Restaurants </option>
+                <option value="option 11">Parks</option>
+                <option value="option 12">Bars</option>
+                <option value="option 13">Karaoke</option>
+                <option value="option 14">Malls</option>
+                <option value="option 15">Amusement Park</option>
+                <option value="option 16">Water Parks</option>
+                <option value="option 17">Landmark & Hills</option>
+                <option value="option 18">Musuem</option>
+                <option value="option 19">Monuments</option>
+                <option value="option 20">Temples</option>
+                <option value="option 21">Malls</option>
+                <option value="option 22">Zoos</option>
+                <option value="option 23">Aquariums</option>
+                <option value="option 24">Art Galleries</option>
+                <option value="option 25">Tourist AttractionsIcon</option>
+                <option value="option 26">Malls</option>
+            </select>
                 </div>
                 <input type="submit" value="Search"/>
             </div>
