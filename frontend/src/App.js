ec072a19bed12db803385cfffeb85a22e918173a
Arnav Chakravarthy <77641677+arnavnc@users.noreply.github.com>
2022-02-21 14:54:56 -0800

    update App.js
    renders all card data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
 import './App.css';
+import Card from './components/Card'
+import attractions from './data'
 
+function createCard(attraction) {
+  return <Card 
+    name = {attraction.name}
+    loc = {attraction.loc}
+    address = {attraction.address}
+    number = {attraction.number}
+    website = {attraction.website}
+    times = {attraction.open_time}
+    rating = {attraction.star_rating}
+    reviews = {attraction.reviews}
+  />
+}
+
+console.log(attractions[0].name)
 
 class App extends Component
 {
@@ -9,7 +25,7 @@ class App extends Component
   {
     return (
       <div className="App">
-        
+        {attractions.map(createCard)}
       </div>
     );  
   }
